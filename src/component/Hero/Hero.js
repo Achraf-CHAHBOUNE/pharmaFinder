59829ee2ee2f804aff808c4a1e1c321e8099248d
Achraf <achrafchahboune8@gmail.com>
2024-05-31 02:19:19 +0100

    I make a modification in tailwind.config.js file

--- src/component/Hero/Hero.js
+++ src/component/Hero/Hero.js
@@ -9,8 +9,15 @@ const Hero = () => {
         className="flex justify-center h-full w-full bg-cover bg-center"
         style={{ backgroundImage: `url(${backgroundImage})` }} // Use the imported image
       >
-        <div className="w-1/2"></div>
-        <div className="w-1/2">
+        <div className="w-2/5 flex flex-col justify-center items-center gap-5 mb-36">
+          <h1 className="text-3xl font-mono font-bold text-teal-500 text-center font-4xl uppercase">
+            Find the best pharmacy near you
+          </h1>
+          <h3 className="text-cblue text-2xl text-center text- w-4/5 font-mono font-bold font-4xl uppercase">
+             in  just a few clicks
+          </h3>
+        </div>
+        <div className="w-3/5">
           <img src={Image} alt="Hero" />
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cblue: "#072264",
+      },
+    },
   },
   plugins: [],
 }
